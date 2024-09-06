1fb19eb3b804e78c6f39176da5154fcf5dff1fda
Raí Augusto <129444550+RaiKnova@users.noreply.github.com>
2024-09-06 13:18:11 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,31 @@
+let dados = [
+    
+     {
+        titulo : "Rayssa Leal",
+        descricao : "A skatista brasileira Rayssa Leal, ou Fadinha, é um ícone do esporte nacional. Com sua habilidade única e carisma inigualável, ela conquistou o mundo do skate e se tornou um dos maiores nomes da modalidade.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate skatista fadinha fada street"
+    },
+
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade representa um marco histórico na ginástica artística brasileira. Sua trajetória, marcada por conquistas inéditas e uma performance técnica impecável, posiciona-a como um dos maiores expoentes da modalidade em âmbito mundial.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags:"ginasta ginástica artistica artística"
+    },
+
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma das maiores promessas do judô brasileiro. Com sua força, técnica e determinação, ela conquistou o país e o mundo com suas medalhas e performances impecáveis.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags:"judo judô judoca judoka"
+    }
+
+]
+
+
+
+
+
+
+
